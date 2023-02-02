410fd36af5ae101c64d820a111dd4f1f8ff1c925
Sokhna Diata BEYE <122283339+ssokhna@users.noreply.github.com>
2023-02-02 13:06:21 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,64 +1,34 @@
-#! /usr/bin/env node
-//shebang
+
+
+function monShell(commande){
+    const { execSync } = require('child_process')
+    return new Promise((resolve,reject) => {
+        execSync(commande,(err, output) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve(output)
+    
+        });
+});
+}
 
 console.log("----------------------------");
 console.log("           SHELL              ");
 console.log("----------------------------");
 
-
 const ps = require("prompt-sync");
 const prompt = ps();
 
 let commande = prompt(">");
+do {
+    if (commande === "exit") {
+        break }
 
-if (commande == "ls -la") {
-    const { exec } = require('child_process')
+    else {
+      console.log(monShell(commande))
+    }
+  
+    commande = prompt(">");
 
-    // lance la commande `ls -la` en utilisant exec
-    
-    exec('ls -la ', (err, output) => {
-    //une fois la commande terminée, la fonction de rappel est appelée
-        if (err) {
-           // enregistre et retourne si nous rencontrons une erreur
-            console.error("could not execute command: ", err)
-            return
-        }
-    // enregistre la sortie reçue de la commande
-        console.log("Output: \n", output)
-    })
-            
-}
-
-else if (commande == "ls") {
-    const { exec } = require("child_process");
-    
-    exec("ls", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });         
-}
-
-else if (commande == "mkdir") {
-    const { exec } = require("child_process");
-    
-    exec("mkdir fichierr", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });         
-}
-
-else(console.log("error !"))
+}while (true)
